a269149a70742812701ca4fd6fbce29a9aba1e0d
User <zoranicerdal73@gmail.com>
2024-02-19 23:46:10 +0100

    Drugi commit

--- src/app/service/customer.service.ts
+++ src/app/service/customer.service.ts
@@ -28,6 +28,7 @@ export class CustomerService {
     return this.http.put(BASIC_URL + "/api/customer/" + id, customer);
   }
 
+  /* Delete Customer */
   deleteCustomer(id: number): Observable<any> {
     return this.http.delete(BASIC_URL + "/api/customer/" + id);
   }
